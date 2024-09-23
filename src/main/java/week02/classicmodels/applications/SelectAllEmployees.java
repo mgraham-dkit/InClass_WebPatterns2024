69fdca1a1b1623e45f6af03a9d765f1a77fd54dc
michelle <michelle.graham@dkit.ie>
2024-09-23 17:29:52 +0100

    Week 2: Removed unused import statements leftover from a previous restructuring.

--- src/main/java/week02/classicmodels/applications/SelectAllEmployees.java
+++ src/main/java/week02/classicmodels/applications/SelectAllEmployees.java
@@ -4,9 +4,6 @@ import week02.classicmodels.business.Employee;
 import week02.classicmodels.persistence.EmployeeDao;
 import week02.classicmodels.persistence.EmployeeDaoImpl;
 
-import java.sql.*;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SelectAllEmployees {
